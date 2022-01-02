6e9e777733ff2431154fb048bcf09b28e5362862
sakshambakshi <sakshambakshi@gmail.com>
2022-01-03 01:11:07 +0530

    init proxy api

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const server = express();
+const cors = require('cors')
+server.use(cors())
+
+server.get("/get/:symbol", async function (req, res) {
+  const axios = require("axios");
+
+  const  config = {
+    method: "get",
+    url: `https://query1.finance.yahoo.com/v8/finance/chart/${req.params.symbol}`,
+    headers: {},
+  };
+
+  const {data} = await axios(config)
+    return res.json(data)
+});
+
+
+server.listen(8000, async () => {
+    console.log("started")
+
+});
